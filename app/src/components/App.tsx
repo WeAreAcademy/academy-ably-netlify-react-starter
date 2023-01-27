107d03bea79c5d578a05cc9e5c794530a51b0301
nbogie <neillbogie@googlemail.com>
2023-01-27 21:48:06 +0000

    tidy button fn

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -2,10 +2,9 @@ import { configureAbly, useChannel } from "@ably-labs/react-hooks";
 import { Types } from "ably/promises";
 import React, { useState } from "react";
 
-import './App.css';
 import { MessageView } from "./MessageView";
 
-const optionalClientId = "neillReactClient"; // When not provided in authUrl, a default will be used.
+const optionalClientId = "academyReactClient"; // When not provided in authUrl, a default will be used.
 configureAbly({ authUrl: `/api/ably-token-request?clientId=${optionalClientId}` });
 
 function App() {
@@ -17,13 +16,18 @@ function App() {
     setMessages(prev => [...prev, msg]);
   })
 
+  function handleVoteClick() {
+    console.log("attempting to publish...")
+    channel.publish("one-vote", {
+      candidate: 17,
+      message: "Hello world!"
+    });
+  }
+
   return (
     <div className="App">
-      <button onClick={() => {
-        console.log("attempting to publish...")
-        channel.publish("one-vote", { message: "Hello world!" });
-      }
-      }>Click to publish</button>
+      <button onClick={handleVoteClick
+      }>Click to publish a vote!</button>
       <hr />
       {messages.map(msg => <MessageView message={msg} />)}
     </div >
